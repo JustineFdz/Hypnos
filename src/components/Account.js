487d025bc4b3ec682cb4bad83fdea971de5239fe
Mon MacBook <justine.fernandez1990@gmail.com>
2022-04-25 16:32:11 +0200

    error localhost

--- src/components/Account.js
+++ src/components/Account.js
@@ -19,7 +19,7 @@ export const Account = () => {
     const userId = sessionStorage.getItem("userId");
     if (isLoggedIn) {
       axios
-        .get(`http://localhost:3001${userId}`)
+        .get(`https://hypnos-booking-backend.herokuapp.com/${userId}`)
         .then((response) => {
           console.log('>>>>>>>>>> booking Date',response.data)
           setListOfBookings(response.data);
